10b83f5e5aaccc4092a88fe249105dccfdbeec2f
Eric Gelinas <eric@standardpixel.com>
2013-01-14 20:17:58 -0800

    finish record CRUD stuff

--- create-record.js
+++ create-record.js
@@ -3,64 +3,15 @@ var colors     = require('colors'),
 	sdb        = require(__dirname + '/simpledb_client.js').client,
     sys        = require('util'),
 	record_key = process.argv[2],
-	record_val = process.argv[3]
+	record_val = JSON.parse(process.argv[3]);
 
 console.log('\033[2J');
 console.log('Create record'.bold.underline);
 
-console.log('record', record_key, record_val);
-
-sdb.select('select $ItemName,created_at from twitter_favs where created_at is not null order by created_at desc limit 1', function(error,response) {
-	
-	/*
+sdb.putItem(keys.aws.domain,keys.aws.record_prefix + record_key,record_val,function(error,data) {
 	if(error) {
-		console.error(error);
+		console.log(('AWS Response Error ' + error.Message).red, error.Message);
 	} else {
-		for(var i=0, l=favs.length;l>i;i++) {
-					
-			if(response.length < 1 || (new Date(favs[i].created_at).getTime() > response[0].created_at)) {
-				if(favs[i] && favs[i].id_str) {
-					sdb.getItem('twitter_favs', favs[i].id_str, {sp_iteration:i}, function(error, data, meta) {
-						var iteration = meta.query.sp_iteration;
-
-						if(error) {
-							console.log(('AWS Request Error ' + error.Message).red, error.Message);
-						} else {
-							if(data) {
-								too_new_message++;
-							} else {
-								sdb.putItem('twitter_favs',favs[iteration].id_str,{
-									text                         : favs[iteration].text,
-									screen_name                  : favs[iteration].user.screen_name,
-									profile_image_url            : favs[iteration].user.profile_image_url,
-									profile_background_image_url : favs[iteration].user.profile_background_image_url,
-									created_at                   : new Date(favs[iteration].created_at).getTime(),
-									entities                     : favs[iteration].entities.urls
-								},function(error,data) {
-									if(error) {
-										console.log(('AWS Response Error ' + error.Message).red, error.Message, '(' + favs[iteration].id + '/' + favs[iteration].user.screen_name + ')');
-									} else {
-										console.log(('Added a tweet from ' + favs[iteration].user.screen_name).green);
-									}
-								});
-							}
-						}
-					});
-				}
-			} else {
-				too_new_message++;
-			}
-					
-			//
-			// Messages
-			//
-			if(i === favs.length-1) {
-				if(too_new_message) {
-					console.log(('Skipping ' +too_new_message+ ' tweets because they are older than the last stored tweet').blue);
-				}
-			}
-					
-		}
+		console.log('Added'.green);
 	}
-	*/
 });
